5e4a1517173beaf74d7c9cc85e705cdab62efd67
Renuka Choudhary <renurunrun@gmail.com>
2023-05-20 17:37:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import UserData from './Components/UserData';
+
+import Comments from './Components/Comments';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path='/' element={<UserData/>}/>
+      <Route path='/comments/:id' element={<Comments/>}/>
+      <Route path='*' element={<UserData/>}/>
+    </Routes>
+   
     </div>
   );
 }
---
+++ src/Components/Comments.js
@@ -0,0 +1,51 @@
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {useParams} from 'react-router-dom'
+const Comments = () => {
+    const {id} = useParams();
+    const [data, setData] = useState([])
+    
+    useEffect(()=>{
+       axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`).then((res)=>{
+        console.log(res.data)
+        setData(res.data);
+         
+       })
+        // eslint-disable-next-line
+    },[])
+    return(
+        <>
+        
+        <table className="table">
+        <thead>
+          <tr>
+          <th> Id</th>
+          <th>User Id</th>
+          <th>Title</th>
+          <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+        {// eslint-disable-next-line
+          data.map((elem,idx) => {
+              return(
+                <tr key={idx}>
+                  <td>{elem.id}</td>
+                  <td>{elem.postId}</td>
+                  <td>{elem.name}</td>
+                  <td>{elem.email}</td>
+                  
+                </tr>
+              )
+              
+          })
+        }
+        </tbody>
+      </table>
+        </>
+    )
+}
+
+export default Comments
